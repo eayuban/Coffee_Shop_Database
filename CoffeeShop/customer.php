eed0cca7fe5514f6e17a420d2d60d0d542b65e07
aerjay <aerjay.italia@icloud.com>
2017-04-08 15:33:37 -0600

    Added modifyoage for manager

--- CoffeeShop/customer.php
+++ CoffeeShop/customer.php
@@ -26,15 +26,12 @@
         <li> </li>
       </ul>
       <form class="navbar-form navbar-right" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
+        <div class="form-group"> </div>
+</form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a> </li>
-        <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
+        <li> </li>
+        <li class="dropdown">
+<ul class="dropdown-menu">
             <li><a href="#">Action</a> </li>
             <li><a href="#">Another action</a> </li>
             <li><a href="#">Something else here</a> </li>
--- CoffeeShop/employee.php
+++ CoffeeShop/employee.php
@@ -41,13 +41,9 @@
                     </form>
                     <ul class="nav navbar-nav navbar-right">
                         <li><a href="#">Link</a> </li>
-                        <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">Dropdown <span class="caret"></span></a>
+                        <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">Modify <span class="caret"></span></a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Action</a> </li>
-                                <li><a href="#">Another action</a> </li>
-                                <li><a href="#">Something else here</a> </li>
-                                <li role="separator" class="divider"></li>
-                                <li><a href="#">Separated link</a> </li>
+                                <li><a href="modifypage.php">Recipe/Equiptment/Inventory</a> </li>
                             </ul>
                         </li>
                     </ul>

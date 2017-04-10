2eb6477891a2e0aec42421d20233c973c8b3445c
r-mcnally <rowan.mcnally@ucalgary.ca>
2017-04-10 15:11:12 -0600

    Update addsale.php

--- CoffeeShop/addsale.php
+++ CoffeeShop/addsale.php
@@ -18,6 +18,7 @@
             
 
             addSale($conn);
+            updatePoints($conn);
             closeDatabaseConn($conn);
             
             function addSale($conn){
@@ -40,7 +41,67 @@
                     echo "Sale of $drinkType was not added successfully...<br>".$conn->error."<br>";
                 }
             }
-
+        
+            function updatePoints($conn){
+                echo "Updating points balance of $_POST[custEmail] in the database!<br>";
+                
+                $drinkType = $_POST[drinkType];
+                $custEmail = $_POST[custEmail];
+                
+                //determine points to add
+                $sql="SELECT Price 
+                      FROM drink 
+                      WHERE Name='$drinkType'";
+                
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0){
+                    
+                    echo "got inside price<br>";
+                    
+                    $row = $result->fetch_assoc();
+                    $price = $row["Price"];
+                    
+                    echo "price is $price<br>";
+                    
+                    $multiplier = 100;
+                    $pointsToAdd = (int) $price * $multiplier;
+                    
+                    echo "Points to add $pointsToAdd<br>";
+                    
+                    $sql = "SELECT Points_Balance
+                            FROM customer
+                            WHERE Email='$custEmail'";
+                    $result = $conn->query($sql);
+                    
+                    if ($result->num_rows > 0){
+                        
+                        echo "got inside pull points balance<br>";
+                        $row = $result->fetch_assoc();
+                        $currentBalance=$row["Points_Balance"];
+                        
+                        echo "current balance is $currentBalance<br>";
+                        
+                        $newPoints = $currentBalance + $pointsToAdd;
+                        
+                        echo"new points $newPoints<br>";
+                        
+                        $sql = "UPDATE customer
+                            SET Points_Balance= '$newPoints'
+                            WHERE Email = '$custEmail'";
+                        
+                        if($conn->query($sql) == TRUE){
+                            echo "Points Balance of $custEmail was successfully updated!<br>";
+                        }
+                        else{
+                            echo "Points balance of $custEmail was not updated successfully...<br>".$conn->error."<br>";
+                        }
+                    }
+                }
+                else{
+                    echo "Points balance of $custEmail was not updated successfully...<br>".$conn->error."<br>";
+                }
+                
+            }
         function connectToDatabase($server, $user, $psswrd, $database){
             $conn = new mysqli($server, $user, $psswrd, $database);
             

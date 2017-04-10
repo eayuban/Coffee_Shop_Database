a9a00047aef5e6f34261fe7374665175b161a80d
r-mcnally <rowan.mcnally@ucalgary.ca>
2017-04-10 10:15:37 -0600

    Update addRemoveDrink.php

--- CoffeeShop/addRemoveDrink.php
+++ CoffeeShop/addRemoveDrink.php
@@ -16,11 +16,15 @@
             
             $conn = connectToDatabase($servername, $username, $password, $db);
             
-            if (strcmp($_POST[AddOrRemove], "Add") == 0){
+            if (strcmp($_POST[AddOrUpdateOrRemove], "Add") == 0){
                 echo "<h1>You are here because you ADDED a DRINK to the Database</h1>";
                 addDrink($conn);
             }
-            else{
+            if (strcmp($_POST[AddOrUpdateOrRemove], "Update") == 0){
+                echo "<h1>You are here because you UPDATED a DRINK in the Database</h1>";
+                updateDrink($conn);
+            }
+            if(strcmp($_POST[AddOrUpdateOrRemove], "Remove") == 0){
                 echo "<h1>You are here because you REMOVED a DRINK from the Database</h1>";
 
                 removeDrink($conn);
@@ -48,7 +52,55 @@
                     echo "$drinkName was not added successfully...<br>".$conn->error."<br>";
                 }
             }
-            
+            function updateDrink($conn){
+                echo "Updating $_POST[drinkName] in the database!<br>";
+                
+                $drinkName = $_POST[drinkName];
+                
+                //verify that the drink is in the database
+                $sql = "SELECT 
+                        *
+                        FROM drink
+                        WHERE
+                        Name = '$drinkName'";
+                $result = $conn->query($sql);
+           
+                if ($result->num_rows > 0){
+                  
+                    $row = $result->fetch_assoc();
+                    
+                    $recipe = $_POST[recipe];
+                    $type = $_POST[drinkType];
+                    $price = $_POST[price];
+                    
+                    if(empty($_POST[recipe])){
+                    
+                        $recipe = $row["Recipe"];
+                    }
+                    if(empty($_POST[drinkType])){
+                      
+                        $type = $row["Type"];
+                    }
+                    if(empty($_POST[price])){
+                     
+                        $price = $row["Price"];
+                    }
+                    $sql = "UPDATE drink
+                            SET Recipe='$recipe',Type='$type',Price='$price'
+                            WHERE 
+                            Name = '$drinkName'";  
+                    if($conn->query($sql) == TRUE){
+                    echo "Updated $drinkName successfully!<br>";
+                }
+                else{
+                    echo "$drinkName was not updated successfully...<br>".$conn->error."<br>";
+                }
+                }
+                else{
+                    echo "Invalid drink name<br>".$conn->error."<br>";
+                }
+                
+                        
             function removeDrink($conn){
             
                 echo "Removing $_POST[drinkName] from the database!<br>";
